92f20766d800b5286aa27bc5c32bf3b6f798258d
Ray <rbelhajjannus@cifpfbmoll.eu>
2026-02-23 20:08:32 +0100

    Inicializando Spring Boot

---
+++ FCT/src/main/java/IFC33B/FCT/GestorDePracticasApplication.java
@@ -0,0 +1,13 @@
+package IFC33B.FCT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDePracticasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDePracticasApplication.class, args);
+	}
+
+}
