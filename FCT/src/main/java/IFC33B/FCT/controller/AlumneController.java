f602c6f3376f1c960974834e01847591b1b81896
Luis-Pozo-Benz <lpozobenz@cifpfbmoll.eu>
2026-02-23 20:45:31 +0100

    Add missing put endpoint

--- FCT/src/main/java/IFC33B/FCT/controller/AlumneController.java
+++ FCT/src/main/java/IFC33B/FCT/controller/AlumneController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,6 +23,11 @@ public class AlumneController {
 
     @PostMapping
     public int createAlumne() {
+        return 0;
+    }
+
+    @PutMapping
+    public int updateAlumne() {
         return 0;
     }
 
