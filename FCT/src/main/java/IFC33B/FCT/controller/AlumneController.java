b8f9b0b31b7ce0a1e13270aa0918d2bb5df28c6e
Luis-Pozo-Benz <lpozobenz@cifpfbmoll.eu>
2026-02-23 20:41:56 +0100

    Completed AlumneController endpoint templates

--- FCT/src/main/java/IFC33B/FCT/controller/AlumneController.java
+++ FCT/src/main/java/IFC33B/FCT/controller/AlumneController.java
@@ -1,7 +1,9 @@
 package IFC33B.FCT.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,6 +12,21 @@ public class AlumneController {
     
     @GetMapping
     public int getAllAlumnes() {
+        return 0;
+    }
+
+    @GetMapping("/{id}")
+    public int getAlumneById() {
+        return 0;
+    }
+
+    @PostMapping
+    public int createAlumne() {
+        return 0;
+    }
+
+    @DeleteMapping
+    public int deleteAlumne() {
         return 0;
     }
 }
