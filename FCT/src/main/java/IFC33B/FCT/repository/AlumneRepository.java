6a90c457bc62ce8b7f2a7bc47e4956838e68697d
mateolopezlorenz <mlopezlorenz@cifpfbmoll.eu>
2026-02-23 20:58:51 +0100

    Modified AlumneRepository

--- FCT/src/main/java/IFC33B/FCT/repository/AlumneRepository.java
+++ FCT/src/main/java/IFC33B/FCT/repository/AlumneRepository.java
@@ -1,9 +1,16 @@
 package IFC33B.FCT.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import IFC33B.FCT.model.Alumne;
 
 public interface AlumneRepository extends JpaRepository<Alumne, Long> {
-    
 
+    // Funcions per a trobar un alumne per el seu nom, per l'id, el objette alumne, i si existeix o no.
+
+    Optional<Alumne> findById(Long id);
+    Optional<Alumne> findByName(String name);
+    boolean existsByName(String name);
+    Optional<Alumne> findAlumne(String name);
 }
