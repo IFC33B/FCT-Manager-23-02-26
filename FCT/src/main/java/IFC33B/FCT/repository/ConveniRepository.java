4f5f0942e4fea16b9a10b53eef4f0a027afe91b0
evivesheynecifp <evivesheyne@cifpfbmoll.eu>
2026-02-24 16:53:40 +0100

    Fix conveni repository
    - Rename variables
    - Fix import

--- FCT/src/main/java/IFC33B/FCT/repository/ConveniRepository.java
+++ FCT/src/main/java/IFC33B/FCT/repository/ConveniRepository.java
@@ -3,9 +3,12 @@ package IFC33B.FCT.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import IFC33B.FCT.model.Convenio;
+
+import IFC33B.FCT.model.Conveni;
 
 public interface ConveniRepository extends JpaRepository<Conveni, Long> {
-    Optional<Conveni> findById(Integer Id);
-    boolean existsById(Integer Id);
+
+    Optional<Convenis> findById(Integer id);
+
+    boolean existsById(Integer id);
 }
