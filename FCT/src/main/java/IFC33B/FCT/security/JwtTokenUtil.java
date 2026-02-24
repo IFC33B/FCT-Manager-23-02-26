721d7ce8f232c5ff01ff1369199c966f9214c379
SvenLee-J <sjansengorreta@cifpfbmoll.eu>
2026-02-24 16:07:40 +0100

    Comentarios en el archivo JwtTokenUtil.java

--- FCT/src/main/java/IFC33B/FCT/security/JwtTokenUtil.java
+++ FCT/src/main/java/IFC33B/FCT/security/JwtTokenUtil.java
@@ -1,4 +1,4 @@
-package IFC33B.FCT.Controllers;
+package IFC33B.FCT.security;
 
 import java.security.Key;
 import java.sql.Date;
@@ -12,29 +12,36 @@ import org.springframework.stereotype.Component;
 @Component
 public class JwtTokenUtil {
     
+    // Clave Secreta para tokens
     @Value("${jwt.secret}")
     private String secret;
     
+    // Tiempo de expiracion del token
     @Value("${jwt.expiration}")
     private Long expiration;
     
+    // Genera las claves.
     private Key getSigningKey() {
         return Keys.hmacShaKeyFor(secret.getBytes());
     }
     
+    // Extrae el nombre del usuario del token
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
     
+    // Extrae la fecha de expiracion.
     public Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
     
+    // Funcion que extrae los tokens
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
     
+    // Extar los datos del token
     private Claims extractAllClaims(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(getSigningKey())
@@ -43,15 +50,18 @@ public class JwtTokenUtil {
                 .getBody();
     }
     
+    // Boolean que indica si el token expiro
     private Boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
     
+    // Genera el token
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
         return createToken(claims, userDetails.getUsername());
     }
     
+    // Construye los tokens.
     private String createToken(Map<String, Object> claims, String subject) {
         return Jwts.builder()
                 .setClaims(claims)
@@ -62,6 +72,7 @@ public class JwtTokenUtil {
                 .compact();
     }
     
+    // Valida que el token.
     public Boolean validateToken(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
